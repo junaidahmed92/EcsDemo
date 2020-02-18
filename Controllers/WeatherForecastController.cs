e4f3a71037181ffde0472f284f02709b451ddc31
junaidahmed92 <Junaid.Ahmed@atea.com>
2020-02-18 16:29:33 +0500

    MinorChange

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -29,7 +29,7 @@ namespace EcsDemo.Controllers
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = DateTime.Now.AddDays(index+1),
+                Date = DateTime.Now.AddDays(index+2),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
